a3b17358ca29c742188b9fc5f77e714efeaf1f29
Rodrigo Oliva <roliva@fortinet-us.com>
2025-07-03 01:42:11 -0400

    Pipeline 0207205 v6

--- main.tf
+++ main.tf
@@ -52,7 +52,37 @@ resource "aws_instance" "apache_server" {
               yum install -y httpd
               systemctl enable httpd
               systemctl start httpd
-              echo "¡Servidor Apache funcionando en EC2!" > /var/www/html/index.html
+              # Descargar imagen (reemplaza la URL con tu imagen real)
+              curl -o /var/www/html/fortinet-heroes.png https://github.com/roliva-fortinet/xperience-caso2/blob/main/heroes.png
+
+              # Crear el archivo index.html con fondo
+              cat <<EOT > /var/www/html/index.html
+              <!DOCTYPE html>
+              <html>
+              <head>
+                <title>Fortinet Superhéroes</title>
+                <style>
+                  body {
+                    margin: 0;
+                    padding: 0;
+                    background-image: url('fortinet-heroes.png');
+                    background-size: cover;
+                    background-position: center;
+                    height: 100vh;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    color: white;
+                    font-family: Arial, sans-serif;
+                    text-shadow: 2px 2px 4px #000;
+                  }
+                </style>
+              </head>
+              <body>
+                <h1>¡FortiCNAPP, FortiGate VM y FortiAppSec protegiendo la nube!</h1>
+              </body>
+              </html>
+              EOT
               EOF
 
   tags = {
